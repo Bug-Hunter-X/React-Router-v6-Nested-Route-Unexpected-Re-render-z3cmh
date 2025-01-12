9be4a16e65c3dd936c8a4c8548ab274e9b7ca5c4
BugHunter-X <askzenai@gmail.com>
2025-01-12 07:58:08 +0100

    Add App.js file

---
+++ App.js
@@ -0,0 +1,24 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* This route is causing the issue */}
+        <Route path="/contact" element={<Contact />} >
+          <Route path="/:id" element={<ContactDetails />} />
+        </Route> 
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() { return <h1>Home</h1>; }
+function About() { return <h1>About</h1>; }
+function Contact() { return <h1>Contact</h1>; }
+function ContactDetails() { return <h1>Contact Details</h1>; }
+export default App;
+```
